b0dff82b2f962e57437ebbe507ca37096ed7e7a7
bojidarapartenova <bpartenova@gmail.com>
2024-11-29 19:44:48 +0200

    Add another excercise

---
+++ Inheritance - Exercise/restaurant/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            
+            switch(product)
+            {
+                case "Coffee":
+                    {
+                        string name = Console.ReadLine();
+                        double caffeine=double.Parse(Console.ReadLine());
+
+                        Coffee coffee=new Coffee(name, caffeine);
+
+                        Console.WriteLine(coffee);
+                    }
+                    break;
+            }
+        }
+    }
+}
